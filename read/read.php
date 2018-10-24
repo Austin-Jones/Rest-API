099627061974e6645d3236ea6f7e685bf50ee31a
Austin <96austin.j@gmail.com>
2018-10-24 10:10:05 -0400

    Update read.php

--- read/read.php
+++ read/read.php
@@ -25,9 +25,7 @@ if($num > 0){
     $orders_arr=array();
     $orders_arr["orders"]=array();
  
-    // retrieve our table contents
-    // fetch() is faster than fetchAll()
-    // http://stackoverflow.com/questions/2770630/pdofetchall-vs-pdofetch-in-a-loop
+    // retrieve table contents
     while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
         // extract row
         // this will make $row['name'] to
